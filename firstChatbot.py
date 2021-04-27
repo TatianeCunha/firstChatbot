f95e7dd62e273a9f0240415c18d2e40fedb00e4f
Tatiane.cunha <tati_diascunha@hotmail.com>
2021-04-27 08:53:02 -0300

    including login and database connection

--- firstChatbot.py
+++ firstChatbot.py
@@ -1,32 +1,47 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+import mysql.connector
+import psycopg2
 
-bot = ChatBot('Irineu')
-bot = ChatBot(
-    'Irineu',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri='sqlite:///database.sqlite3'
-    )
-    
-conversa = ListTrainer(bot)
-conversa.train([
-    'Oi?',
-    'Eae',
-    'Qual o seu nome?',
-    'Irineu, você não sabe e nem eu',
-    'eu sou burro',
-    'Prazer em te conhecer',
-    'Igualmente meu patrão',
-    'burro',
-    'Tom Holland fala espanhol?',
-    'não'
-])
-while True:
-    try:
-        resposta = bot.get_response(input("Usuário: "))
-        if float(resposta.confidence) > 0.5:
-            print("Irineu: ", resposta)
-        else:
-            print("Eu não entendi :(")
-    except(KeyboardInterrupt, EOFError, SystemExit):
-        break
+#connection to the database
+mydb = psycopg2.connect(
+    host="tuffi.db.elephantsql.com",
+    database="uevnqhtj",
+    user="uevnqhtj",
+    password="RqAmmvdUX0q2vR_cJh9pnQ9uAkfLW213",
+    port='5432')
+
+cursor = mydb.cursor()
+
+#chatbot
+
+print("Chatbot: Oi! Seja bem-vindo! Para começarmos, preciso de algumas informações suas.")
+print("Chatbot: Pode me informar seu nome?")
+nome = input()
+
+print("Chatbot: Certo, e qual a sua idade?")
+idade=input()
+
+print("Chatbot: Qual o seu sexo? Feminino ou masculino?")
+sexo=input()
+
+print("Chatbot: Pode nos contar um pouco mais sobre você? Possui alguma doença crônica?")
+doencaCronica=input()
+
+print("Chatbot: Possui alguma alergia?")
+alergia=input()
+
+print("Chatbot: e histórico de cirurgias?")
+cirurgia=input()
+
+print("Chatbot: Estou procurando o especialista que possa melhor te atender")
+
+
+cursor.execute("insert into paciente values (nextval('seq_paciente'), '"
+    +nome+"','"
+    +idade+"','"
+    +sexo+"','"
+    +doencaCronica+"','"
+    +alergia+"','"
+    +cirurgia+ "')")
+mydb.commit()
+
+print(cursor.rowcount, "was inserted.")
---
+++ server.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+#Broadcast
+
+def broadcast (message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} disse {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+#Receive Function
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Conectado com {str(address)}!")
+
+        client.send("NOME ".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"O nome do usuário é {nickname}")
+        broadcast(f"{nickname} conectado com o Chatbot!\n".encode('utf-8'))
+        client.send("Conectado com o Chatbot ".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args =(client,))
+        thread.start()
+
+#Handle
+
+print("Servidor rodando...")
+receive()
